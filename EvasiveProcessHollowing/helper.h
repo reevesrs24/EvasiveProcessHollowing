36e3842d496693a83f53f28f6efce04f1a3f0619
reevesrs24 <reevesrs24@gmail.com>
2019-12-30 01:43:18 -0500

    Initial Commit

---
+++ EvasiveProcessHollowing/helper.h
@@ -0,0 +1,209 @@
+#pragma once
+#include <wtypes.h>
+
+
+
+
+typedef enum PROCESSINFOCLASS
+{
+	ProcessBasicInformation = 0, // 0, q: PROCESS_BASIC_INFORMATION, PROCESS_EXTENDED_BASIC_INFORMATION
+	ProcessQuotaLimits, // qs: QUOTA_LIMITS, QUOTA_LIMITS_EX
+	ProcessIoCounters, // q: IO_COUNTERS
+	ProcessVmCounters, // q: VM_COUNTERS, VM_COUNTERS_EX
+	ProcessTimes, // q: KERNEL_USER_TIMES
+	ProcessBasePriority, // s: KPRIORITY
+	ProcessRaisePriority, // s: ULONG
+	ProcessDebugPort, // q: HANDLE
+	ProcessExceptionPort, // s: HANDLE
+	ProcessAccessToken, // s: PROCESS_ACCESS_TOKEN
+	ProcessLdtInformation, // 10
+	ProcessLdtSize,
+	ProcessDefaultHardErrorMode, // qs: ULONG
+	ProcessIoPortHandlers, // (kernel-mode only)
+	ProcessPooledUsageAndLimits, // q: POOLED_USAGE_AND_LIMITS
+	ProcessWorkingSetWatch, // q: PROCESS_WS_WATCH_INFORMATION[]; s: void
+	ProcessUserModeIOPL,
+	ProcessEnableAlignmentFaultFixup, // s: BOOLEAN
+	ProcessPriorityClass, // qs: PROCESS_PRIORITY_CLASS
+	ProcessWx86Information,
+	ProcessHandleCount, // 20, q: ULONG, PROCESS_HANDLE_INFORMATION
+	ProcessAffinityMask, // s: KAFFINITY
+	ProcessPriorityBoost, // qs: ULONG
+	ProcessDeviceMap, // qs: PROCESS_DEVICEMAP_INFORMATION, PROCESS_DEVICEMAP_INFORMATION_EX
+	ProcessSessionInformation, // q: PROCESS_SESSION_INFORMATION
+	ProcessForegroundInformation, // s: PROCESS_FOREGROUND_BACKGROUND
+	ProcessWow64Information, // q: ULONG_PTR
+	ProcessImageFileName, // q: UNICODE_STRING
+	ProcessLUIDDeviceMapsEnabled, // q: ULONG
+	ProcessBreakOnTermination, // qs: ULONG
+	ProcessDebugObjectHandle, // 30, q: HANDLE
+	ProcessDebugFlags, // qs: ULONG
+	ProcessHandleTracing, // q: PROCESS_HANDLE_TRACING_QUERY; s: size 0 disables, otherwise enables
+	ProcessIoPriority, // qs: ULONG
+	ProcessExecuteFlags, // qs: ULONG
+	ProcessResourceManagement,
+	ProcessCookie, // q: ULONG
+	ProcessImageInformation, // q: SECTION_IMAGE_INFORMATION
+	ProcessCycleTime, // q: PROCESS_CYCLE_TIME_INFORMATION
+	ProcessPagePriority, // q: ULONG
+	ProcessInstrumentationCallback, // 40
+	ProcessThreadStackAllocation, // s: PROCESS_STACK_ALLOCATION_INFORMATION, PROCESS_STACK_ALLOCATION_INFORMATION_EX
+	ProcessWorkingSetWatchEx, // q: PROCESS_WS_WATCH_INFORMATION_EX[]
+	ProcessImageFileNameWin32, // q: UNICODE_STRING
+	ProcessImageFileMapping, // q: HANDLE (input)
+	ProcessAffinityUpdateMode, // qs: PROCESS_AFFINITY_UPDATE_MODE
+	ProcessMemoryAllocationMode, // qs: PROCESS_MEMORY_ALLOCATION_MODE
+	ProcessGroupInformation, // q: USHORT[]
+	ProcessTokenVirtualizationEnabled, // s: ULONG
+	ProcessConsoleHostProcess, // q: ULONG_PTR
+	ProcessWindowInformation, // 50, q: PROCESS_WINDOW_INFORMATION
+	ProcessHandleInformation, // q: PROCESS_HANDLE_SNAPSHOT_INFORMATION // since WIN8
+	ProcessMitigationPolicy, // s: PROCESS_MITIGATION_POLICY_INFORMATION
+	ProcessDynamicFunctionTableInformation,
+	ProcessHandleCheckingMode,
+	ProcessKeepAliveCount, // q: PROCESS_KEEPALIVE_COUNT_INFORMATION
+	ProcessRevokeFileHandles, // s: PROCESS_REVOKE_FILE_HANDLES_INFORMATION
+	MaxProcessInfoClass
+};
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+
+typedef struct _PEB_LDR_DATA {
+	BYTE       Reserved1[8];
+	PVOID      Reserved2[3];
+	LIST_ENTRY InMemoryOrderModuleList;
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
+
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+	BYTE           Reserved1[16];
+	PVOID          Reserved2[10];
+	UNICODE_STRING ImagePathName;
+	UNICODE_STRING CommandLine;
+} RTL_USER_PROCESS_PARAMETERS, * PRTL_USER_PROCESS_PARAMETERS;
+
+typedef struct _PEB_FREE_BLOCK
+{
+	_PEB_FREE_BLOCK *Next;
+	ULONG Size;
+} PEB_FREE_BLOCK, *PPEB_FREE_BLOCK;
+
+
+typedef struct _ACTIVATION_CONTEXT_DATA { void* dummy; } ACTIVATION_CONTEXT_DATA;
+typedef struct _ASSEMBLY_STORAGE_MAP { void* dummy; } ASSEMBLY_STORAGE_MAP;       
+typedef struct _FLS_CALLBACK_INFO { void* dummy; } FLS_CALLBACK_INFO;             
+
+
+typedef struct _PEB
+{
+	UCHAR InheritedAddressSpace;
+	UCHAR ReadImageFileExecOptions;
+	UCHAR BeingDebugged;
+	UCHAR BitField;
+	ULONG ImageUsesLargePages : 1;
+	ULONG IsProtectedProcess : 1;
+	ULONG IsLegacyProcess : 1;
+	ULONG IsImageDynamicallyRelocated : 1;
+	ULONG SpareBits : 4;
+	PVOID Mutant;
+	PVOID ImageBaseAddress;
+	PPEB_LDR_DATA Ldr;
+	PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+	PVOID SubSystemData;
+	PVOID ProcessHeap;
+	PRTL_CRITICAL_SECTION FastPebLock;
+	PVOID AtlThunkSListPtr;
+	PVOID IFEOKey;
+	ULONG CrossProcessFlags;
+	ULONG ProcessInJob : 1;
+	ULONG ProcessInitializing : 1;
+	ULONG ReservedBits0 : 30;
+	union
+	{
+		PVOID KernelCallbackTable;
+		PVOID UserSharedInfoPtr;
+	};
+	ULONG SystemReserved[1];
+	ULONG SpareUlong;
+	PPEB_FREE_BLOCK FreeList;
+	ULONG TlsExpansionCounter;
+	PVOID TlsBitmap;
+	ULONG TlsBitmapBits[2];
+	PVOID ReadOnlySharedMemoryBase;
+	PVOID HotpatchInformation;
+	VOID** ReadOnlyStaticServerData;
+	PVOID AnsiCodePageData;
+	PVOID OemCodePageData;
+	PVOID UnicodeCaseTableData;
+	ULONG NumberOfProcessors;
+	ULONG NtGlobalFlag;
+	LARGE_INTEGER CriticalSectionTimeout;
+	ULONG HeapSegmentReserve;
+	ULONG HeapSegmentCommit;
+	ULONG HeapDeCommitTotalFreeThreshold;
+	ULONG HeapDeCommitFreeBlockThreshold;
+	ULONG NumberOfHeaps;
+	ULONG MaximumNumberOfHeaps;
+	VOID** ProcessHeaps;
+	PVOID GdiSharedHandleTable;
+	PVOID ProcessStarterHelper;
+	ULONG GdiDCAttributeList;
+	PRTL_CRITICAL_SECTION LoaderLock;
+	ULONG OSMajorVersion;
+	ULONG OSMinorVersion;
+	WORD OSBuildNumber;
+	WORD OSCSDVersion;
+	ULONG OSPlatformId;
+	ULONG ImageSubsystem;
+	ULONG ImageSubsystemMajorVersion;
+	ULONG ImageSubsystemMinorVersion;
+	ULONG ImageProcessAffinityMask;
+	ULONG GdiHandleBuffer[34];
+	PVOID PostProcessInitRoutine;
+	PVOID TlsExpansionBitmap;
+	ULONG TlsExpansionBitmapBits[32];
+	ULONG SessionId;
+	ULARGE_INTEGER AppCompatFlags;
+	ULARGE_INTEGER AppCompatFlagsUser;
+	PVOID pShimData;
+	PVOID AppCompatInfo;
+	UNICODE_STRING CSDVersion;
+	_ACTIVATION_CONTEXT_DATA* ActivationContextData;
+	_ASSEMBLY_STORAGE_MAP* ProcessAssemblyStorageMap;
+	_ACTIVATION_CONTEXT_DATA* SystemDefaultActivationContextData;
+	_ASSEMBLY_STORAGE_MAP* SystemAssemblyStorageMap;
+	ULONG MinimumStackCommit;
+	_FLS_CALLBACK_INFO* FlsCallback;
+	LIST_ENTRY FlsListHead;
+	PVOID FlsBitmap;
+	ULONG FlsBitmapBits[4];
+	ULONG FlsHighIndex;
+	PVOID WerRegistrationData;
+	PVOID WerShipAssertPtr;
+} PEB, *PPEB;
+
+typedef struct _PROCESS_BASIC_INFORMATION {
+	PVOID Reserved1;
+	PPEB PebBaseAddress;
+	PVOID Reserved2[2];
+	ULONG_PTR UniqueProcessId;
+	PVOID Reserved3;
+} PROCESS_BASIC_INFORMATION;
+
+typedef NTSTATUS(WINAPI* _ZwUnmapViewOfSection)(
+	_In_     HANDLE ProcessHandle,
+	_In_opt_ PVOID  BaseAddress
+	);
+
+typedef NTSTATUS(WINAPI* _NtQueryInformationProcess)(
+	IN HANDLE           ProcessHandle,
+	IN PROCESSINFOCLASS ProcessInformationClass,
+	OUT PVOID           ProcessInformation,
+	IN ULONG            ProcessInformationLength,
+	OUT PULONG          ReturnLength
+	);
